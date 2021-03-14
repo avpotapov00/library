01c284dc896037c300f63d712c23833c159d96a1
qwake1378 <45040429+qwake1378@users.noreply.github.com>
2021-03-14 04:16:15 +0300

    css removed

--- src/main/java/info/potapov/library/config/SwaggerConfig.java
+++ src/main/java/info/potapov/library/config/SwaggerConfig.java
@@ -12,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 class SwaggerConfig {
 
+
     @Bean
     Docket configure() {
         return new Docket(DocumentationType.SWAGGER_2)
